ba552af6d16818eb80bb4261eee99b33bf96b05a
Jan Weiß <weiss19ja@gmail.com>
2015-05-05 09:28:41 +0200

    Added gitignore

--- FixMyMail/ViewController.swift
+++ FixMyMail/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var bls:MCOIMAPSession;
     }
 
     override func didReceiveMemoryWarning() {
