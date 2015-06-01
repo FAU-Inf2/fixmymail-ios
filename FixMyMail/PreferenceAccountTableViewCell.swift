b1317870421a9093f82a8152c68abff15fb710a7
Sebastian Thürauf <mcqstream.me@gmail.com>
2015-06-01 11:31:40 +0200

    added PreferenceAccountTableViewCell

---
+++ FixMyMail/PreferenceAccountTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PreferenceAccountTableViewCell.swift
+//  FixMyMail
+//
+//  Created by Sebastian Thürauf on 01.06.15.
+//  Copyright (c) 2015 FixMymail. All rights reserved.
+//
+
+import UIKit
+
+class PreferenceAccountTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var textfield: TextfieldWithPadding!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
