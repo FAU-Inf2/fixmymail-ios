e55abfd203c00ab4fbe582914f1353a2df6fff14
Tobias Werth <werth@cs.fau.de>
2015-06-12 17:56:33 +0200

    fixing upload script

--- fastlane/uploadLog.sh
+++ fastlane/uploadLog.sh
@@ -1,5 +1,5 @@
-if [ -f "../$LOG" ]
+if [ -f "../$LOG_FILE" ]
 then
-  echo "Uploading log file $LOG"
-  scp ../$LOG mad:/var/www/smile/log
+  echo "Uploading log file $LOG_FILE"
+  scp ../$LOG_FILE mad:/var/www/smile/log
 fi
