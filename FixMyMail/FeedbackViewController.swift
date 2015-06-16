91392d03255ab38c062b14a1dc790120d82e77f6
Sebastian Thürauf <sebastian.thuerauf@me.com>
2015-06-16 21:24:17 +0200

    Feedback: added action to "send feedback" button, uses standardaccount if set, else uses first fetched email account. if no account is set user is promted to add an account or use the link in the textview, added email link to feedback.html so the user can use standard apple mail app to send feedback (in the case SMile is not working correctly)

--- FixMyMail/FeedbackViewController.swift
+++ FixMyMail/FeedbackViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
-class FeedbackViewController: UIViewController {
+class FeedbackViewController: UIViewController, NSFetchedResultsControllerDelegate{
 
 	@IBOutlet weak var textView: UITextView!
 	@IBOutlet weak var buttonFeedback: UIButton!
+	
+	var preferences: Preferences?
+	var accounts: [EmailAccount]?
+
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,8 @@ class FeedbackViewController: UIViewController {
 			let attributedString = NSAttributedString(fileURL: html, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil, error: nil)
 			self.textView.attributedText = attributedString
 		}
+		
+		self.loadData()
 		
 
         // Do any additional setup after loading the view.
@@ -31,6 +38,48 @@ class FeedbackViewController: UIViewController {
     }
     
 	@IBAction func buttonFeedbackTapped(sender: UIButton) {
+		var standardAccount: EmailAccount?
+		var recipient = MCOAddress(mailbox: "fixmymail@i2.cs.fau.de")
+		var subject = "Feedback for SMile on iOS"
+		var sendView = MailSendViewController(nibName: "MailSendViewController", bundle: nil)
+		sendView.recipients.addObject(recipient)
+		sendView.subject = subject
+		
+		if self.accounts!.count != 0 {
+			if self.preferences != nil {
+				if self.preferences?.standardAccount != "" {
+					for account in self.accounts! {
+						if account.accountName == self.preferences?.standardAccount {
+							standardAccount = account
+						}
+					}
+					
+					// user has declared a standard account -> use it
+					if standardAccount != nil {
+						sendView.account = standardAccount!
+						self.navigationController?.pushViewController(sendView, animated: true)
+					}
+					
+					
+				} else {
+					// user has NOT declared a standard account
+					sendView.account = self.accounts?.first
+					self.navigationController?.pushViewController(sendView, animated: true)
+				}
+			} else {
+				// got accounts but no preferences
+				
+				sendView.account = self.accounts?.first
+				self.navigationController?.pushViewController(sendView, animated: true)
+			}
+			
+		} else {
+			// user has no accounts declared
+			var alert = UIAlertController(title: "Sorry", message: "It seems you have not declared an Email account, please use the provided link or add a new account in Preferences > Accounts > \"Add New Account\".", preferredStyle: UIAlertControllerStyle.Alert)
+			alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in }))
+			self.presentViewController(alert, animated: true, completion: nil)
+			
+		}
 	}
 
     /*
@@ -43,4 +92,47 @@ class FeedbackViewController: UIViewController {
     }
     */
 
+	
+	func loadData() {
+		let appDel: AppDelegate? = UIApplication.sharedApplication().delegate as? AppDelegate
+		if let appDelegate = appDel {
+			var managedObjectContext = appDelegate.managedObjectContext
+			var preferencesFetchRequest = NSFetchRequest(entityName: "Preferences")
+			var error: NSError?
+			let fetchedPreferences: [Preferences]? = managedObjectContext!.executeFetchRequest(preferencesFetchRequest, error: &error) as? [Preferences]
+			
+			if let preferences = fetchedPreferences {
+				self.preferences = preferences[0]
+			} else {
+				if((error) != nil) {
+					NSLog(error!.description)
+				}
+			}
+		}
+		
+		self.accounts = self.getAccount()
+		
+	}
+	
+	func getAccount() -> [EmailAccount]? {
+		var managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext as NSManagedObjectContext!
+		var retaccount = [EmailAccount]()
+		let fetchRequest: NSFetchRequest = NSFetchRequest(entityName: "EmailAccount")
+		var error: NSError?
+		var result = managedObjectContext!.executeFetchRequest(fetchRequest, error: &error)
+		if error != nil {
+			NSLog("%@", error!.description)
+		} else {
+			if let emailAccounts = result {
+				for account in emailAccounts {
+					
+					retaccount.append(account as! EmailAccount)
+					
+				}
+			}
+		}
+		
+		return retaccount
+	}
+	
 }
