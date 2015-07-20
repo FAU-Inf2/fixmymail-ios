2c807ac808e2ec06d93bc7618ab186115dbf88ac
Tobias Werth <werth@cs.fau.de>
2015-07-20 11:40:47 +0200

    fixing deploy script

--- fastlane/deploy.sh
+++ fastlane/deploy.sh
@@ -1,7 +1,7 @@
 if [ -f "../SMile.ipa" ]
 then
   echo "Uploading ipa"
-  scp ../SMile.ipa mad:/var/www/hockey/public/de.fau.cs.mad.SMile.ios/SMile.ipa
+  scp ../SMile.ipa mad:/var/www/hockey/public/de.fau.cs.mad.smile.ios/smile.ipa
 else
   echo "Ipa file not found! :-("
 fi
