d73c60263b5eae0cc63b6d515922e4e01e001a11
Sebastian Thürauf <sebastian.thuerauf@me.com>
2015-08-17 22:07:10 +0200

    implemented KeyDetailTableView, changed KeyChainListTableView appearance and navigation to KeyDetailView

--- SMile/KeyChainListTableViewController.swift
+++ SMile/KeyChainListTableViewController.swift
@@ -13,7 +13,7 @@ class KeyChainListTableViewController: UITableViewController {
 	
 	var delegate: ContentViewControllerProtocol?
 	var navController: UINavigationController!
-	var keyDetailView: KeyDetailViewController?
+	var keyDetailVC: KeyDetailTableViewController?
 	var keyList = [Key]()
 	var keysFromCoreData = [Key]()
 	var managedObjectContext: NSManagedObjectContext?
@@ -118,8 +118,6 @@ class KeyChainListTableViewController: UITableViewController {
 		}
 		
 		
-		
-		
 		// Set the valid thru bar
 		let currentDate = NSDate()
 		if keyItem.validThru.year() >= currentDate.year() {
@@ -150,65 +148,18 @@ class KeyChainListTableViewController: UITableViewController {
 		return cell
 	}
 	
-	
-	
-	
-	/*
-	// Override to support conditional editing of the table view.
-	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-	// Return NO if you do not want the specified item to be editable.
-	return true
-	}
-	*/
-	
-	/*
-	// Override to support editing the table view.
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-	if editingStyle == .Delete {
-	// Delete the row from the data source
-	tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-	} else if editingStyle == .Insert {
-	// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-	}
-	}
-	*/
-	
-	/*
-	// Override to support rearranging the table view.
-	override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-	
-	}
-	*/
-	
-	/*
-	// Override to support conditional rearranging of the table view.
-	override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-	// Return NO if you do not want the item to be re-orderable.
-	return true
-	}
-	*/
-	
-	/*
-	// MARK: - Navigation
-	
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-	// Get the new view controller using [segue destinationViewController].
-	// Pass the selected object to the new view controller.
-	}
-	*/
-	/*
-	override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-	var detailview: KeyDetailViewController = KeyDetailViewController()
-	self.presentViewController(detailview, animated: true) { () -> Void in
-	
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		let cell = tableView.dequeueReusableCellWithIdentifier("ListPrototypCell", forIndexPath: indexPath) as! KeyItemTableViewCell
+		var keyItem = self.keyList[indexPath.row]
+		self.keyDetailVC = KeyDetailTableViewController(nibName: "KeyDetailTableViewController", bundle: nil)
+		self.keyDetailVC?.keyItem = keyItem
+		self.navigationController?.pushViewController(self.keyDetailVC!, animated: true)
+		
+		tableView.deselectRowAtIndexPath(indexPath, animated: true)
 	}
 	
 	
-	}
-	*/
-	
-	func loadInitialData() {
+	private func loadInitialData() {
 		
 		let appDel: AppDelegate? = UIApplication.sharedApplication().delegate as? AppDelegate
 		if let appDelegate = appDel {
